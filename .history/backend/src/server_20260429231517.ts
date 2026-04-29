db84b8ce0fa7b0ece604de2addb407c415406b79
dipto230 <diptoroy230305@gmail.com>
2026-04-29 23:22:58 +0530

    project backend initialized done

---
+++ .history/backend/src/server_20260429231517.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Backend server is running 🚀");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
